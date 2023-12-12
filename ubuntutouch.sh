14592ac70e7453aa5cfa9a3a26ac903e82089e33
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2023-12-12 10:49:48 -0800

    Update ubuntutouch.sh

--- ubuntutouch.sh
+++ ubuntutouch.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-#for use on Pixel 3 Ubuntu Touch as hardware( instead of Raspberry Pi or other SBC)
 
 sudo mount -o remount,rw /
 sudo wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
+sudo apt-get update
+sudo apt-get purge --auto-remove python-blinker
+sudo apt install git-all
 sudo python3 -m pip install git+https://github.com/npinto/opencv.git
 sudo python3 -m pip install flask
