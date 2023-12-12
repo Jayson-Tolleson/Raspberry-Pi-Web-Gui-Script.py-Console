60fd573fcc1ca820a639e89a5ee72b1eb381ed66
Jayson-Tolleson <30577311+Jayson-Tolleson@users.noreply.github.com>
2023-12-12 14:37:56 -0800

    Update ubuntutouch.sh

--- ubuntutouch.sh
+++ ubuntutouch.sh
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 sudo mount -o remount,rw /
 sudo wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
-sudo apt-get update
-sudo apt-get purge --auto-remove python-blinker
-sudo apt install git-all
-sudo python3 -m pip install git+https://github.com/npinto/opencv.git
-sudo python3 -m pip install flask
+sudo python3 -m pip3 install flask
+sudo apt update
+sudo apt install git
+sudo git clone https://github.com/npinto/opencv.git
+cd opencv
+sudo make
+sudo make install
+cd ..
+sudo echo "[Unit]
+Description=Custom Python Service
+After=multi-user.target
+Conflicts=getty@tty1.service
+[Service]
+Type=simple
+ExecStart=/usr/bin/python3 ~/Documents/CommandConsole/console.py
+StandardInput=tty-force
+[Install]
+WantedBy=multi-user.target" >  /lib/systemd/system/console.service
+sudo systemctl daemon-reload
+sudo systemctl enable console.service
+sudo systemctl start console.service
+sudo python3 console.py
